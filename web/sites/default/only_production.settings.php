729853dc176c7a05c2db8fb7f06b58911c65cdaf
dni <daniele@nicastro.io>
2020-05-20 21:02:05 +0200

    config sync

--- web/sites/default/only_production.settings.php
+++ web/sites/default/only_production.settings.php
@@ -12,6 +12,11 @@ $settings['hash_salt'] = '749ab2c0d06c42ae3b841b79e79875f02b3a042e43c92378cd28bd
 
 $settings['config_sync_directory'] = '../config/sync';
 
+$settings['trusted_host_patterns'] = [
+  '^' . str_replace(['.', 'https://', 'http://', ','], ['\.', '', '', '|'], 'ricardoanalytic.seauton.io') . '$',
+  // escape dots, remove schema, use commas as regex separator
+];
+
 // Don't show any error messages on the site (will still be shown in watchdog)
 $config['system.logging']['error_level'] = 'hide';
 
