2d8f0c027b698e9255364963b49183db4e55f57f
dni <daniele@nicastro.io>
2020-05-20 21:31:38 +0200

    trigger #

--- web/index.php
+++ web/index.php
@@ -11,6 +11,11 @@
 use Drupal\Core\DrupalKernel;
 use Symfony\Component\HttpFoundation\Request;
 
+
+
+
+
+
 $autoloader = require_once 'autoload.php';
 
 $kernel = new DrupalKernel('prod', $autoloader);
