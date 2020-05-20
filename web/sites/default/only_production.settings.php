34a9e73731403166b12378279f509375c4f66b85
dni <daniele@nicastro.io>
2020-05-21 00:35:12 +0200

    driver sql

--- web/sites/default/only_production.settings.php
+++ web/sites/default/only_production.settings.php
@@ -44,10 +44,10 @@ $databases['default']['default'] = [
   'database' => 'ricardoanalytic',
   'username' => 'ricardoanalytic',
   'password' => 'MzZTt7mJM',
-  'host' => 'localhost',
+  'host' => '127.0.0.1',
   'port' => 3306,
   'prefix' => '',
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
 ];
-
 
 
