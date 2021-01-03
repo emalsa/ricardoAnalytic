dc954bcd514522a9ec4a4e9aaf5991efcef3270e
dni <daniele@nicastro.io>
2021-01-03 12:42:38 +0100

    subdirectory

--- web/sites/default/production.settings.php
+++ web/sites/default/production.settings.php
@@ -23,6 +23,10 @@ $settings['config_sync_directory'] = '../config/sync';
 //  '*$',
 //];
 
+if (isset($GLOBALS['request']) && '/web/index.php' === $GLOBALS['request']->server->get('SCRIPT_NAME')) {
+  $GLOBALS['request']->server->set('SCRIPT_NAME', '/index.php');
+}
+
 $settings['hash_salt'] = 'whatever-i-like-29292929';
 $settings['file_temp_path'] = '/tmp';
 $settings['file_public_path'] = 'sites/default/files';
