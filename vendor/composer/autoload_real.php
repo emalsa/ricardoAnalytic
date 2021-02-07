143f6a041408d11615374c9346a449240319095c
dni <daniele@nicastro.io>
2021-02-07 13:05:10 +0100

    composer update

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -13,6 +13,9 @@ class ComposerAutoloaderInitcd402dc517bcdf28de939ad6a23972de
         }
     }
 
+    /**
+     * @return \Composer\Autoload\ClassLoader
+     */
     public static function getLoader()
     {
         if (null !== self::$loader) {
